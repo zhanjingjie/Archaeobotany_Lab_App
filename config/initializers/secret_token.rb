5356cedbccec6cf51cb7d022d9bbc2daa1df8079
Jingjie Zhan <jingjiezhan.jj@gmail.com>
2013-02-15 15:12:35 -0800

    Initial commit with rspec framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArchaeobotanyLabApp::Application.config.secret_token = '6a014f5c784f1455143b3860e65732d00f624c753fac9de031143d8acde2c98d14b9695c95a69e94e6128459727e9b13f44219764317bb187abc32a433667970'
